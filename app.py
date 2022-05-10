209a8c3098a3f42358633b563c360290ddb34ff8
Nick Hsiao <hsiao.nicholas@gmail.com>
2022-05-10 14:29:10 -0700

    forwarding message functionality complete

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 from flask import Flask, Response, request
 from twilio.twiml.messaging_response import MessagingResponse
-
+from twilio.rest import Client
+import os
+from phone_numbers import number_list
 app = Flask(__name__)
+
+account_sid = os.environ.get('TWILIO_ID')
+auth_token  = os.environ.get('TWILIO_AUTH')
+client = Client(account_sid, auth_token)
+
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     resp = MessagingResponse()
-    
-    resp.message("Hi there")
-    
+    body = request.values.get('Body')
 
+    forward_sms(body)
+    
     return Response(str(resp),mimetype="application/xml")
+
+def forward_sms(msg):
+    
+    for number in number_list:
+        print(number)
+        client.messages.create(
+        to=number, 
+        from_=os.environ.get('TWILIO_NUMBER'),
+        body=msg)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
