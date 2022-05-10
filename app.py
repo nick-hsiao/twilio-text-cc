b608f8f7f6f371764e61ea5167586af82d676bfd
Nick Hsiao <hsiao.nicholas@gmail.com>
2022-05-10 13:53:14 -0700

    basic text response

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 from flask import Flask, Response, request
-from twilio import twiml
-import os
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-print(os.environ)
-
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    number = request.form['From']
-    body = request.form['Body']
+    resp = MessagingResponse()
     
-    resp = twiml.Response()
-    print(body)
+    resp.message("Hi there")
+    
 
-    return str(resp)
+    return Response(str(resp),mimetype="application/xml")
 
 if __name__ == "__main__":
     app.run(debug=True)
